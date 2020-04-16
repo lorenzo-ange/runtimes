4e4911cbf9640dae8a45d22e408ad34248b6797e
Lorenzo Angelini <lorenzo.angelini@innoteam.it>
2020-04-16 15:34:59 +0200

    add DB migrations execution

--- stable/dotnetcore/compile-function.sh
+++ stable/dotnetcore/compile-function.sh
@@ -11,3 +11,17 @@ USER_PROJ=$PROJECT_MOUNT/*proj
 # compile
 dotnet restore $USER_PROJ --packages $PACKAGES_DIR
 dotnet publish $USER_PROJ -o publish -c Release --no-restore
+
+# execute Entity Framework Core DB migrations (if any)
+cd $PROJECT_MOUNT
+if [ -d "Migrations" ] 
+then
+    echo "DB migrations execution started"
+    dotnet tool install --global dotnet-ef
+    export PATH="$PATH:/root/.dotnet/tools"
+    dotnet add package Microsoft.EntityFrameworkCore.Design -v 3.1.2
+    # dotnet ef database update
+    echo "DB migrations execution ended"
+else
+    echo "No DB migrations found"
+fi
