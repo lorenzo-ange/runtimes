11a8b2b28b604f99e4e08fc2230d50285a73f8cb
Lorenzo Angelini <lorenzo.angelini@innoteam.it>
2020-06-17 15:22:04 +0200

    reduce logging for Microsoft and System namespaces

--- stable/dotnetcore/src/Kubeless.WebAPI/Program.cs
+++ stable/dotnetcore/src/Kubeless.WebAPI/Program.cs
@@ -2,6 +2,7 @@ using System;
 using Kubeless.WebAPI.Utils;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Sentry.Extensibility;
 
 namespace Kubeless.WebAPI
@@ -17,6 +18,11 @@ namespace Kubeless.WebAPI
         {
             var port = VariablesUtils.GetEnvVar("FUNC_PORT", "8080");
             return Host.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddFilter("Microsoft", LogLevel.Warning);
+                    logging.AddFilter("System", LogLevel.Warning);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     var sentryEnabled = !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("SENTRY_DSN"));
