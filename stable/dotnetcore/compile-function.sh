d159afbe2230a642c7063e62aff6c8a1b5ccf245
Lorenzo Angelini <lorenzo.angelini@innoteam.it>
2020-09-03 10:12:25 +0200

    fix compile-function with exit on error

--- stable/dotnetcore/compile-function.sh
+++ stable/dotnetcore/compile-function.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+exit_on_error() {
+    exit_code=$?
+    last_command=$(fc -ln -1 | awk '{$1=$1};1')
+    if [ $exit_code -ne 0 ]; then
+        echo "\"${last_command}\" command failed with exit code ${exit_code}."
+        exit $exit_code
+    fi
+}
+
 # kubeless function directory
 PROJECT_MOUNT=$1
 echo Mount $PROJECT_MOUNT
@@ -10,7 +19,9 @@ USER_PROJ=$PROJECT_MOUNT/*proj
 
 # compile
 dotnet restore $USER_PROJ --packages $PACKAGES_DIR
+exit_on_error
 dotnet publish $USER_PROJ -o publish -c Release --no-restore
+exit_on_error
 
 # execute Entity Framework Core DB migrations (if any)
 cd $PROJECT_MOUNT
@@ -19,12 +30,9 @@ then
     echo "DB migrations execution started"
     echo "CONNECTION_STRING='$CONNECTION_STRING'"
     dotnet add package Microsoft.EntityFrameworkCore.Design -v $EF_CORE_VERSION
-    if ! dotnet ef database update; then
-        echo "DB migration failed"
-        exit 1
-    else
-        echo "DB migrations execution ended"
-    fi
+    exit_on_error
+    dotnet ef database update
+    exit_on_error
 else
     echo "No DB migrations found"
 fi
